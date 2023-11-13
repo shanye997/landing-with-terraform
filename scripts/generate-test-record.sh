81aaa1749e5a062605b2986210b6d62d199fa454
shanye <shanye.xzq@alibaba-inc.com>
2023-11-13 10:16:22 +0800

    ci: add pr e2e and weekly e2e test

---
+++ scripts/generate-test-record.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "false" ];then
+  exit 0
+fi
+
+$f=$2
+success=true
+if [ -n "$3" ] ;then
+  success=false
+fi
+
+fileName=$(basename $f)
+testRecordFile=TestRecord/$fileName/TestRecord.md.tmp
+if [ ! -d "TestRecord/${fileName}" ]; then
+  mkdir -p TestRecord/$fileName
+  touch $testRecordFile
+fi
+
+time=$(date -u "+%d %b %Y %H:%M UTC")
+echo -e "## $time\n" > $testRecordFile
+echo -e "success: ${success}\n\n### Versions\n" >> $testRecordFile
+
+row=$(terraform version | sed -n '/^$/=')
+allRow=$(terraform version | sed -n '$=')
+
+if [ $row -ne $allRow ]; then
+  version=$(terraform version | sed -n "1,${row}p")
+fi
+echo ${version} >> $testRecordFile
+echo -e "\n### Error\n" >> $testRecordFile
+
+if [ -n "$3" ] ;then
+  echo $3 >> $testRecordFile
+fi
